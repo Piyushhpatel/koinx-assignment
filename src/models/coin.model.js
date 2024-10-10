a07d0e9bb43f2667760b8175892183b1fae6dcfa
Piyush Patel <piyushpatel1746@gmail.com>
2024-10-10 12:08:03 +0530

    Added Coin Models

---
+++ src/models/coin.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const coinSchema = new mongoose.Schema(
+    {
+        coinId: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        currentPrice: {
+            type: Number,
+            required: true
+        },
+        marketCap: {
+            type: Number,
+            required: true
+        },
+        Change24h: {
+            type: Number,
+            required: true
+        }
+    }, 
+    {timestamps: true}
+);
+
+export const Coin = mongoose.model("Coin", coinSchema);
